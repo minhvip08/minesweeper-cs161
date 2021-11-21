147bc048d2229552db65c36534bd09b8ebf0936b
minhvip08 <minhvip08@gmail.com>
2021-11-21 22:36:49 +0700

    change Sth

--- cheat.cpp
+++
@@ -1,9 +0,0 @@
-#include"header.h"
-// A Function to cheat by revealing where the mines are
-// placed.
-void cheatMinesweeper(char realBoard[][MAXSIDE])
-{
-	printf("The mines locations are-\n");
-	printBoard(realBoard);
-	return;
-}
--- cheat.h
+++
@@ -1,4 +0,0 @@
-// A Function to cheat by revealing where the mines are
-// placed.
-#include"header.h"
-void cheatMinesweeper(char realBoard[][MAXSIDE]);
